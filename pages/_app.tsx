5142042f4bf63c5dc8c47c8f7006e39f0e74f054
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-03 22:16:56 +0530

    feat: Enhanced pages/_app.tsx,  - Detailed changes: diff --git a/pages/_app.tsx b/pages/_app.tsx index... at 01:58 PM +0530, July 03, 2025

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
+import { AuthProvider } from '../src/contexts/AuthContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
