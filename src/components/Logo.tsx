eede5b86f74b53062447a9f8d40e6ccb3fea716b
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-03 21:02:52 +0530

    feat: Enhanced src/components/Logo.tsx,  - Detailed changes: diff --git a/src/components/Logo.tsx b/src/compone... at 01:58 PM +0530, July 03, 2025

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -13,12 +13,11 @@ const Logo: React.FC<LogoProps> = ({
   showUnderline = false,
   interactive = false 
 }) => {
-
   const sizeClasses = {
     small: 'text-sm sm:text-base',
     medium: 'text-lg sm:text-xl lg:text-2xl',
-    large: 'text-xl sm:text-2xl lg:text-3xl xl:text-4xl',
-    xl: 'text-2xl sm:text-3xl lg:text-4xl xl:text-5xl'
+    large: 'text-xl sm:text-2xl lg:text-3xl',
+    xl: 'text-2xl sm:text-3xl lg:text-4xl'
   };
 
   const logoClass = `logo-font font-bold transition-all duration-300 ${sizeClasses[size]} ${className} ${
@@ -26,31 +25,16 @@ const Logo: React.FC<LogoProps> = ({
   }`;
 
   return (
-    <div className={logoClass}>
-      <span 
-        className="relative inline-block"
-        style={{ color: '#FF5E14' }}
-      >
-        Tool
-        {showUnderline && (
-          <svg className="absolute -bottom-1 sm:-bottom-2 left-0 w-full h-2 sm:h-3" viewBox="0 0 100 10" style={{ fill: '#FF5E14', opacity: 0.3 }}>
-            <path d="M0 8 Q 50 0 100 8 L 100 10 L 0 10 Z" />
-          </svg>
-        )}
-      </span>
+    <span className={logoClass}>
+      <span style={{ color: '#FF5E14' }}>Tool</span>
       <span style={{ color: '#001554' }}>N</span>
-      <span 
-        className="relative inline-block"
-        style={{ color: '#FF5E14' }}
-      >
-        Task
-        {showUnderline && (
-          <svg className="absolute -bottom-1 sm:-bottom-2 left-0 w-full h-2 sm:h-3" viewBox="0 0 100 10" style={{ fill: '#FF5E14', opacity: 0.3 }}>
-            <path d="M0 8 Q 50 0 100 8 L 100 10 L 0 10 Z" />
-          </svg>
-        )}
-      </span>
-    </div>
+      <span style={{ color: '#FF5E14' }}>Task</span>
+      {showUnderline && (
+        <svg className="absolute -bottom-1 sm:-bottom-2 left-0 w-full h-2 sm:h-3" viewBox="0 0 100 10" style={{ fill: '#FF5E14', opacity: 0.3 }}>
+          <path d="M0 8 Q 50 0 100 8 L 100 10 L 0 10 Z" />
+        </svg>
+      )}
+    </span>
   );
 };
 
