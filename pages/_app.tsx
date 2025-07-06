4c99c1c962d4e8da0ca60fcf3b7decc0ceea34b1
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-07 00:54:03 +0530

    feat: Enhanced pages/_app.tsx,  - Detailed changes: diff --git a/pages/_app.tsx b/pages/_app.tsx index... at 01:58 PM +0530, July 03, 2025

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
+import { auth } from '../src/lib/firebase';
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) console.log('User logged in:', user.uid);
+      else console.log('No user logged in');
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <Component {...pageProps} />
