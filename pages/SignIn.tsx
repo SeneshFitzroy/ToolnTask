ef8d703927bf79134c7155e47cc297ee58133f7a
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-13 22:40:59 +0530

    feat: Enhanced pages/SignIn.tsx,  - Detailed changes: diff --git a/pages/SignIn.tsx b/pages/SignIn.tsx i... at 01:58 PM +0530, July 03, 2025

--- pages/SignIn.tsx
+++ pages/SignIn.tsx
@@ -1,5 +1,3 @@
-// src/pages/SignIn.tsx
-
 import Navigation from '../src/components/Navigation';
 import Footer from '../src/components/Footer';
 import Logo from '../src/components/Logo';
@@ -8,8 +6,7 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { useRouter } from 'next/router';
-// --- CHANGE: Import fetchSignInMethodsForEmail ---
-import { signInWithEmailAndPassword, sendPasswordResetEmail, fetchSignInMethodsForEmail } from 'firebase/auth';
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../src/lib/firebase';
 
 export default function SignIn() {
