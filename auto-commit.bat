f35da10d26a8bd75564ab12417e327755a4c63c9
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-07-18 00:10:59 +0530

    feat: Enhanced auto-commit.bat,  - Detailed changes: diff --git a/auto-commit.bat b/auto-commit.bat ind... at  0:10:59.12 Fri 07/18/2025

--- auto-commit.bat
+++ auto-commit.bat
@@ -1,25 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-
-:: Stage all changes
 git add .
-
-:: Collect changed files and build initial message
 set "FILES="
-for /f "delims=" %%i in ('git diff --cached --name-only') do set "FILES=%%i, !FILES!"
-
-:: Get detailed change statistics
-for /f "tokens=1-3" %%j in ('git diff --cached --shortstat') do (
-    if not "%%j"=="" set "STATS=%%j %%k %%l"
-)
-
-:: Construct a clear, detailed commit message with current timestamp
-set "MSG=feat: Enhanced !FILES:~0,-2! - !STATS! at %time% %date%"
-
-:: Commit with specified author and push if successful
-git commit -m "!MSG!" --author="SeneshFitzroy <10952757@students.plymouth.ac.uk>" || (
-    echo Commit failed, run git commit manually
-    pause
-    exit /b 1
-)
+for /f %%i in ('git diff --cached --name-only') do set "FILES=%%i, !FILES!"
+for /f "delims=" %%j in ('git diff --cached') do set "CHANGES=!CHANGES!%%j "
+set "MSG=feat: Enhanced !FILES! - Detailed changes: !CHANGES:~0,50!... at %time% %date%"
+git commit -m "!MSG!" --author="SeneshFitzroy <10952757@students.plymouth.ac.uk>"
+if !ERRORLEVEL! neq 0 (echo Commit failed, run git commit manually & pause)
 git push origin main
