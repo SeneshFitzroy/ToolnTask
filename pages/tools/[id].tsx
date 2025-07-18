7e8c8fd5e186a18dfcc7d0656c00f1caf813fabf
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-07-18 18:33:26 +0530

    feat: Enhanced pages/tools/[id].tsx,  - Detailed changes: diff --git a/pages/tools/[id].tsx b/pages/tools/[i... at 18:33:26.82 Fri 07/18/2025

--- pages/tools/[id].tsx
+++ pages/tools/[id].tsx
@@ -250,9 +250,6 @@ export default function ToolDetail() {
         }
       `}</style>
       
-      {/* Dynamic Advertisement - Left Side Only */}
-      <DynamicAdvertisement />
-      
       <div className="min-h-screen" style={{ backgroundColor: theme === 'dark' ? '#0C0F16' : '#F2F3F5' }}>
         <Navigation />
       
