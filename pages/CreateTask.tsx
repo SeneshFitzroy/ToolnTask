03ecaf1bc2752c14ab24c12fce3b70664cd7f8dc
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-07-19 04:27:48 +0530

    feat: Enhanced pages/CreateTask.tsx,  - Detailed changes: diff --git a/pages/CreateTask.tsx b/pages/CreateTa... at  4:27:47.90 Sat 07/19/2025

--- pages/CreateTask.tsx
+++ pages/CreateTask.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useTheme } from 'next-themes';
-import Image from 'next/image';
 import Navigation from '../src/components/Navigation';
 import Footer from '../src/components/Footer';
 import { Button } from '../src/components/ui/button';
