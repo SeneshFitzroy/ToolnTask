ecb69c23c4c0648f7abbd87fd0062aacffabf70e
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-07-19 04:30:48 +0530

    feat: Enhanced src/components/Navigation.tsx,  - Detailed changes: diff --git a/src/components/Navigation.tsx b/src/c... at  4:30:48.28 Sat 07/19/2025

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -92,7 +92,7 @@ const Navigation = () => {
       }}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-14">
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-2 flex-shrink-0" onClick={() => {
             setShowProfileDropdown(false);
