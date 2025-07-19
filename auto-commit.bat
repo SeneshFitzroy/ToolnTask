94a05eaa4b3c1c43f93fae643ff9ea7e768c7f54
Mandira-16 <10952854@students.plymouth.ac.uk>
2025-07-19 23:48:26 +0530

    feat: Enhanced pages/test-links.tsx, auto-commit.bat,  - Detailed changes: diff --git a/auto-commit.bat b/auto-commit.bat ind... at 23:48:26.46 Sat 07/19/2025

--- auto-commit.bat
+++ auto-commit.bat
@@ -1,10 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
+git config user.email "10952854@students.plymouth.ac.uk"
+git config user.name "Mandira-16"
 git add .
 set "FILES="
 for /f %%i in ('git diff --cached --name-only') do set "FILES=%%i, !FILES!"
 for /f "delims=" %%j in ('git diff --cached') do set "CHANGES=!CHANGES!%%j "
 set "MSG=feat: Enhanced !FILES! - Detailed changes: !CHANGES:~0,50!... at %time% %date%"
-git commit -m "!MSG!" --author="SeneshFitzroy <10952757@students.plymouth.ac.uk>"
+git commit -m "!MSG!"
 if !ERRORLEVEL! neq 0 (echo Commit failed, run git commit manually & pause)
 git push origin main
