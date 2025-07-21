01d22e075f27b06d0710a22dcf308556e28080f3
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-07-21 20:36:52 +0530

    feat: Enhanced src/contexts/AuthContext.tsx, pages/_app.tsx,  - Detailed changes: diff --git a/pages/_app.tsx b/pages/_app.tsx index... at 20:36:52.86 Mon 07/21/2025

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,26 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
-import { AuthProvider } from '../src/contexts/AuthContext'; // Import the AuthProvider
+import { auth } from '../src/lib/firebase';
 import { useEffect } from 'react';
 import { initializeAllData } from '../src/lib/initializeData';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) console.log('User logged in:', user.uid);
+      else console.log('No user logged in');
+    });
+    
     // Initialize data on app start
     initializeAllData();
+    
+    return () => unsubscribe();
   }, []);
 
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
