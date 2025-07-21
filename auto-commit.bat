aca6b86e0e5e430f40b48ae3f47a74c34dfea458
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-22 03:23:10 +0530

    feat: Enhanced files - Detailed changes: Automated commit at  3:23:10.37 Tue 07/22/2025

--- auto-commit.bat
+++ auto-commit.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
-git config user.email "10952854@students.plymouth.ac.uk"
-git config user.name "Mandira-16"
+git config user.email "10952757@students.plymouth.ac.uk"
+git config user.name "Senesh Fitzroy"
 git add .
 set "FILES="
-for /f %%i in ('git diff --cached --name-only') do set "FILES=%%i, !FILES!"
+for /f "delims=" %%i in ('git diff --cached --name-only') do set "FILES=%%i, !FILES!"
 for /f "delims=" %%j in ('git diff --cached') do set "CHANGES=!CHANGES!%%j "
 set "MSG=feat: Enhanced !FILES! - Detailed changes: !CHANGES:~0,50!... at %time% %date%"
 git commit -m "!MSG!"
