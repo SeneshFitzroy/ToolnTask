824c3f113c9fa1395107d33301f4dc5d827fdfcb
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-22 21:00:29 +0530

    feat: Enhanced auto-commit.bat,  - Detailed changes: diff --git a/auto-commit.bat b/auto-commit.bat ind... at 21:00:29.31 Tue 07/22/2025

--- auto-commit.bat
+++ auto-commit.bat
@@ -9,4 +9,4 @@ for /f "delims=" %%j in ('git diff --cached') do set "CHANGES=!CHANGES!%%j "
 set "MSG=feat: Enhanced !FILES! - Detailed changes: !CHANGES:~0,50!... at %time% %date%"
 git commit -m "!MSG!"
 if !ERRORLEVEL! neq 0 (echo Commit failed, run git commit manually & pause)
-git push origin main
+git push origin 
