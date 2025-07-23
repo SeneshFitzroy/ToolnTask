000b37d18c6676df503787a464b35abf74243229
Senesh Fitzroy <10952757@students.plymouth.ac.uk>
2025-07-23 21:50:10 +0530

    feat: Enhanced next.config.ts,  - Detailed changes: diff --git a/next.config.ts b/next.config.ts index... at 21:50:10.67 Wed 07/23/2025

--- next.config.ts
+++ next.config.ts
@@ -9,14 +9,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack: (config) => {
-    // Exclude backup files from build
-    config.module.rules.push({
-      test: /.*_backup\.(tsx?|jsx?)$/,
-      loader: 'ignore-loader'
-    });
-    return config;
-  },
   images: {
     remotePatterns: [
       {
